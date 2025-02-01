fb550be6312adf1fc97c5085eb28360c9f3993d2
niranjansalunkhe <niranjansalunkhe1717@gmail.com>
2025-02-01 15:35:34 +0530

    first commit

---
+++ build/CMakeFiles/hii.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hii.dir/main.cpp.o"
+  "hii"
+  "hii.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hii.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
